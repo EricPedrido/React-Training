ca2bc8654b30f418dbba3d82779c9bf0cfaaaf6c
Eric Pedrido <ericwashea@gmail.com>
2021-03-03 15:59:39 +1300

    add checkout page and checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,12 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id == cartItemToAdd.id
+    (cartItem) => cartItem.id === cartItemToAdd.id
   );
 
   // if it is already in cart, create a new array, adding 1 to the item to add, while cloning te rest of the cartItems
   if (existingCartItem) {  
     return cartItems.map((cartItem) =>
-      cartItem.id == cartItemToAdd.id
+      cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
